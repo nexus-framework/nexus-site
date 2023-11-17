224d9c57661e61ecad7eff631395d4741942958b
Afroze Amjad <afroze.amjad@confiz.com>
2023-11-17 13:05:11 +0500

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<AppBar>
+		<svelte:fragment slot="lead">
+			<a class="text-xl uppercase" href="/"><strong>Nexus Framework</strong></a>
+		</svelte:fragment>
+		<svelte:fragment slot="trail">
+			<LightSwitch />
+			<a href="https://docs.nexus-framework.net/"><i class="fa-solid fa-book text-xl" /></a>
+			<a href="https://github.com/nexus-framework"><i class="fa-brands fa-github text-xl" /></a>
+		</svelte:fragment>
+	</AppBar>
+
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Nexus Framework</h2>
+		<div class="space-y-2">
+			<p>
+				Nexus is a Framework for easily setting up microservice solutions for .NET using Steeltoe
+			</p>
+		</div>
+	</div>
+</div>
